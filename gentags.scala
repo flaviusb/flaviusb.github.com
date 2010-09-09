a86d303c1428df923ea761a0c35bc571d883fd62
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-09-10 01:15:27 +1200

    Changed over to use Liquid for almost all the heavy lifting of generating per-tag pages. It is a pity, really...

--- gentags.scala
+++ gentags.scala
@@ -52,7 +52,7 @@ object gentags {
       }
     }
     println(tags2posts)
-    for((tag, posts) <- tags2posts) {
+    /*for((tag, posts) <- tags2posts) {
       var tagfile = new File("tags/" + tag + ".html")
       var fout = new FileWriter(tagfile);
       val page = """
@@ -78,6 +78,82 @@ object gentags {
 </html>
 """
 //"""
+      fout.write(page)
+      fout.flush
+      fout.close
+    }*/
+    for((tag, posts) <- tags2posts) {
+      var tagfile = new File("tags/" + tag + ".xml")
+      var fout = new FileWriter(tagfile);
+      var page = """
+---
+layout: nil
+tag: '"""+tag+"""'
+---
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+ 
+ <title>Justin (:flaviusb) Marsh</title>
+ <link href="http://flaviusb.net/tags/{{ page.tag }}.xml" rel="self"/>
+ <link href="http://flaviusb.net"/>
+ <updated>{{ site.time | date_to_xmlschema }}</updated>
+ <id>http://flaviusb.net/tags/{{ page.tag }}</id>
+ <author>
+   <name>Justin (:flaviusb) Marsh</name>
+   <email>justinius.marsh@gmail.com</email>
+ </author>
+
+ {% for post in site.posts %}
+ {% if post.tags contains page.tag %}
+ <entry>
+   <title>{{ post.title }}</title>
+   <link href="http://flaviusb.net{{ post.url }}"/>
+   <updated>{{ post.date | date_to_xmlschema }}</updated>
+   <id>http://flaviusb.net{{ post.id }}</id>
+   <content type="html">{{ post.content | xml_escape }}</content>
+ </entry>
+ {% endif %}
+ {% endfor %}
+ 
+</feed>"""
+      fout.write(page)
+      fout.flush
+      fout.close
+      tagfile = new File("tags/" + tag + ".html")
+      fout = new FileWriter(tagfile);
+      page = """
+---
+layout: nil
+tag: '"""+tag+"""'
+---
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Pages tagged {{ page.tag }}</title>
+    <link rel="stylesheet" type="text/css" href="http://flaviusb.net/style.css"/>
+    <link rel="stylesheet" type="text/css" href="http://flaviusb.net/container.css"/>
+    <link rel="stylesheet" type="text/css" href="http://flaviusb.net/mono.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="http://flaviusb.net/favicon.ico" type="image/x-icon">
+    <link href='http://flaviusb.net/tags/{{ page.tag }}.xml'  type="application/atom+xml" rel="alternate" title='Blog Atom Feed for tag: {{ page.tag }}' />
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+  </head>
+  <body>
+    <ul class="posts">
+    {% for post in site.posts %}
+    {% if post.tags contains page.tag %}
+      <li><span>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ post.url }}">{{ post.title }}</a> &nbsp; &nbsp; <span>{% for tag in post.tags %} <a href="http://flaviusb.net/tags/{{ tag }}">{{ tag }}</a> &nbsp; {% endfor %}</span></li>
+    {% endif %}
+    {% endfor %}
+    </ul><br>
+
+    <a href="http://flaviusb.net">Home</a>   |   <a href="http://github.com/flaviusb">Code</a>
+  </body>
+</html>
+"""
       fout.write(page)
       fout.flush
       fout.close
